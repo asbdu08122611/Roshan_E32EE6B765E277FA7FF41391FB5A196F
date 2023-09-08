45e04be817a587e3b83399cfb824dd791e2bef4c
asbdu08122611 <144412060+asbdu08122611@users.noreply.github.com>
2023-09-08 10:20:40 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,13 @@
+
+
+def isleapyear(year):
+  if(year%4==0 and year%100!=0):
+   return True
+  else:
+   return false 
+year=int(input("enter a year:"))
+if isleapyear(year):
+  print('{} is a leapyear.'.format(year))
+else:
+  print('{} is not a leapyear.'.format(year))
+  
